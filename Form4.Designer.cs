66f83f68506b960f8b13e8f8a8156efff5308180
Enzo <enzosantiagodesouza@gmail.com>
2025-08-26 20:24:41 -0300

    8 - Criando e configurando a frmAbonoSalarial

---
+++ Form4.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace _3BimAtividadeApp
+{
+    partial class frmAbonoSalarial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.lblSalario = new System.Windows.Forms.Label();
+            this.lblSexo = new System.Windows.Forms.Label();
+            this.lblIdade = new System.Windows.Forms.Label();
+            this.lblNSalario = new System.Windows.Forms.Label();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.txtSexo = new System.Windows.Forms.TextBox();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.txtNSalario = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(56, 116);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(328, 24);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Informe o nome do funcionário(a):";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(412, 113);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(304, 29);
+            this.txtNome.TabIndex = 1;
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVoltar.Location = new System.Drawing.Point(674, 380);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(100, 35);
+            this.btnVoltar.TabIndex = 2;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
+            // 
+            // lblSalario
+            // 
+            this.lblSalario.AutoSize = true;
+            this.lblSalario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalario.Location = new System.Drawing.Point(48, 151);
+            this.lblSalario.Name = "lblSalario";
+            this.lblSalario.Size = new System.Drawing.Size(336, 24);
+            this.lblSalario.TabIndex = 3;
+            this.lblSalario.Text = "Informe o salário do funcionário(a):";
+            // 
+            // lblSexo
+            // 
+            this.lblSexo.AutoSize = true;
+            this.lblSexo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSexo.Location = new System.Drawing.Point(64, 186);
+            this.lblSexo.Name = "lblSexo";
+            this.lblSexo.Size = new System.Drawing.Size(320, 24);
+            this.lblSexo.TabIndex = 4;
+            this.lblSexo.Text = "Informe o sexo do funcionário(a):";
+            // 
+            // lblIdade
+            // 
+            this.lblIdade.AutoSize = true;
+            this.lblIdade.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdade.Location = new System.Drawing.Point(56, 221);
+            this.lblIdade.Name = "lblIdade";
+            this.lblIdade.Size = new System.Drawing.Size(326, 24);
+            this.lblIdade.TabIndex = 5;
+            this.lblIdade.Text = "Informe a idade do funcionário(a):";
+            // 
+            // lblNSalario
+            // 
+            this.lblNSalario.AutoSize = true;
+            this.lblNSalario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNSalario.Location = new System.Drawing.Point(85, 267);
+            this.lblNSalario.Name = "lblNSalario";
+            this.lblNSalario.Size = new System.Drawing.Size(297, 24);
+            this.lblNSalario.TabIndex = 6;
+            this.lblNSalario.Text = "Novo salário do funcionário(a):";
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalario.Location = new System.Drawing.Point(412, 148);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(304, 29);
+            this.txtSalario.TabIndex = 7;
+            // 
+            // txtSexo
+            // 
+            this.txtSexo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSexo.Location = new System.Drawing.Point(412, 183);
+            this.txtSexo.Name = "txtSexo";
+            this.txtSexo.Size = new System.Drawing.Size(304, 29);
+            this.txtSexo.TabIndex = 8;
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIdade.Location = new System.Drawing.Point(412, 218);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(304, 29);
+            this.txtIdade.TabIndex = 9;
+            // 
+            // txtNSalario
+            // 
+            this.txtNSalario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNSalario.Location = new System.Drawing.Point(412, 264);
+            this.txtNSalario.Name = "txtNSalario";
+            this.txtNSalario.ReadOnly = true;
+            this.txtNSalario.Size = new System.Drawing.Size(304, 29);
+            this.txtNSalario.TabIndex = 10;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(568, 380);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(100, 35);
+            this.btnCalcular.TabIndex = 11;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(462, 380);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(100, 35);
+            this.btnLimpar.TabIndex = 12;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // frmAbonoSalarial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtNSalario);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.txtSexo);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.lblNSalario);
+            this.Controls.Add(this.lblIdade);
+            this.Controls.Add(this.lblSexo);
+            this.Controls.Add(this.lblSalario);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblNome);
+            this.Name = "frmAbonoSalarial";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Label lblSalario;
+        private System.Windows.Forms.Label lblSexo;
+        private System.Windows.Forms.Label lblIdade;
+        private System.Windows.Forms.Label lblNSalario;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.TextBox txtSexo;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.TextBox txtNSalario;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
---
+++ Form4.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3BimAtividadeApp
+{
+    public partial class frmAbonoSalarial : Form
+    {
+        public frmAbonoSalarial()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            Hide();
+            frmMenu Menu = new frmMenu();
+            Menu.Show();
+        }
+
+        double nsalario;
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double salario, idade;
+            string sexo, nome;
+
+            salario = Convert.ToDouble(txtSalario.Text);
+            idade = Convert.ToDouble(txtIdade.Text);
+            nome = txtNome.Text;
+            sexo = txtSexo.Text;
+
+            if (idade >= 30)
+            {
+                if (sexo.Equals("F"))
+                    nsalario = salario + 200;
+                else if (sexo.Equals("M"))
+                    nsalario = salario + 100;
+            }
+            else
+            {
+                if (sexo.Equals("F"))
+                    nsalario = salario + 150;
+                else if (sexo.Equals("M"))
+                    nsalario = salario + 50;
+            }
+
+            txtNSalario.Text = nsalario.ToString() + " Esse é o novo salário de " + nome;
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNome.Clear();
+            txtSalario.Clear();
+            txtSexo.Clear();
+            txtIdade.Clear();
+            txtNSalario.Clear();
+        }
+    }
+}
